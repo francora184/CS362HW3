853966f5a8f2ae18a8a42352d3c96c39368440f2
Alex Franco <francora@oregonstate.edu>
2021-01-30 17:40:14 -0800

    Assignment 3

---
+++ isLeapYearErrHandling.py
@@ -0,0 +1,15 @@
+year = int(input("Enter a year.\n"))
+
+if((year > 0)):
+  if (year % 4) == 0:
+    if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("The year {0} is a leap year".format(year))
+       else:
+           print("The year {0} is not a leap year".format(year))
+    else:
+       print("The year {0} is a leap year".format(year))
+  else:
+   print("The year {0} is not a leap year".format(year))
+else:
+  print("Enter a positive integer")
